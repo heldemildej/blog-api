cf64e977c3e85d361f7d8b7b658ebdebd1bf52cd
heldemildej <heldemildemuco@gmail.com>
2025-12-15 19:19:34 -0300

    feat: criar entidade Usuario

---
+++ BlogAPI/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApi.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string SenhaHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "Usuario";
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+    }
+}
