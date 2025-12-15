0abc6c55ea3c1dc0093dd004ae4b901c3b33587c
heldemildej <heldemildemuco@gmail.com>
2025-12-15 19:40:31 -0300

    feat: adicionar BlogContext com DbSet de Usuarios

---
+++ BlogAPI/Data/BlogContext.cs
@@ -0,0 +1,12 @@
+﻿using BlogApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI.Data
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
