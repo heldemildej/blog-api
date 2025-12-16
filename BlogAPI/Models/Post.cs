28b88f616d31b50f76b8b16d77ffa2a750c2fc9f
heldemildej <heldemildemuco@gmail.com>
2025-12-16 01:12:59 -0300

    feat: criar entidade Post com relacionamento para Autor

---
+++ BlogAPI/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogAPI.Models
+{
+    public class Post
+    {
+            [Key] // chave primária
+            public int Id { get; set; }
+
+            [Required]
+            public string Titulo { get; set; } = string.Empty;
+
+            [Required]
+            public string Conteudo { get; set; } = string.Empty;
+
+            [Required]
+            [ForeignKey("Autor")]
+            public int AutorId { get; set; }
+            public Autor Autor { get; set; } = null!;
+
+            public string Categoria { get; set; } = string.Empty;
+
+            public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+
+            public int Views { get; set; } = 0;
+     }
+}
