a2046fb42bc722e4b3052adb26cf9ba95ef6f62e
heldemildej <heldemildemuco@gmail.com>
2025-12-16 01:10:43 -0300

    feat: criar entidade Autor com propriedades básicas

--- BlogAPI/Data/BlogContext.cs
+++ BlogAPI/Data/BlogContext.cs
@@ -1,12 +1,21 @@
 ﻿using BlogApi.Models;
+using BlogAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlogAPI.Data
 {
     public class BlogContext : DbContext
     {
-        public BlogContext(DbContextOptions<BlogContext> options) : base(options) { }
+        private readonly IConfiguration _config;
 
+        public BlogContext(DbContextOptions<BlogContext> options, IConfiguration config)
+            : base(options)
+        {
+            _config = config;
+        }
         public DbSet<Usuario> Usuarios { get; set; } = null!;
+        public DbSet<Autor> Autores { get; set; } = null!;
+        public DbSet<Post> Posts { get; set; } = null!;
+
     }
 }
---
+++ BlogAPI/Models/Autor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogAPI.Models
+{
+    public class Autor
+    {
+            [Key]
+            public int Id { get; set; }
+
+            [Required]
+            public string Nome { get; set; } = string.Empty;
+
+            [Required]
+            public string Email { get; set; } = string.Empty;
+
+        // Um autor pode ter vários posts
+        public List<Post> Posts { get; set; } = new List<Post>();
+     
+    }
+}
