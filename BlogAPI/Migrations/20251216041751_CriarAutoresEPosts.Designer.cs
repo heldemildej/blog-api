66f8371e3c121c9b9491483436555729ab5b6e4b
heldemildej <heldemildemuco@gmail.com>
2025-12-16 01:20:46 -0300

    chore: adicionar migration CriarAutoresEPosts e atualizar database

--- BlogAPI/Migrations/20251216041751_CriarAutoresEPosts.Designer.cs
+++ BlogAPI/Migrations/20251216041751_CriarAutoresEPosts.Designer.cs
@@ -4,6 +4,7 @@ using BlogAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogAPI.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20251216041751_CriarAutoresEPosts")]
+    partial class CriarAutoresEPosts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
