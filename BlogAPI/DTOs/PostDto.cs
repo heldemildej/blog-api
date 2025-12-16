b6d62cd25d7d40562a4fbf51a6311c33aa579181
heldemildej <heldemildemuco@gmail.com>
2025-12-16 01:28:08 -0300

    feat: criar DTO para criação de posts

---
+++ BlogAPI/DTOs/PostDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogAPI.DTOs
+{
+    public class PostDto
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public string Conteudo { get; set; } = string.Empty;
+        public int AutorId { get; set; }
+        public string Categoria { get; set; } = string.Empty;
+    }
+}
